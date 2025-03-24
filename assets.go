99a6d9a11addd253a24662cc63c088490bb02bc4
felo <lfalvaz@gmail.com>
2025-03-24 00:36:01 -0500

    1.8 completed

--- assets.go
+++ assets.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"mime"
 	"os"
+	"path/filepath"
+
+	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -9,4 +15,33 @@ func (cfg apiConfig) ensureAssetsDir() error {
 		return os.Mkdir(cfg.assetsRoot, 0755)
 	}
 	return nil
+}
+
+func getAssetPath(videoID uuid.UUID, mediaType string) (string, error) {
+	extension, err := mime.ExtensionsByType(mediaType)
+	if err != nil {
+		return "", err
+	}
+
+	if len(extension) == 0 {
+		return "", errors.New("no extension associated with filetype")
+	}
+	return fmt.Sprintf("%s%s", videoID, extension[0]), nil
+}
+
+func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
+	return filepath.Join(cfg.assetsRoot, assetPath)
+}
+
+func (cfg apiConfig) getAssetURL(assetPath string) string {
+	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
+
+}
+
+func getMediaType(contentType string) (string, error) {
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return "", err
+	}
+	return mediaType, nil
 }
--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +30,77 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory int64 = 10 << 20
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(maxMemory)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Form cant be parsed", err)
+		return
+	}
+
+	uploadedThumbnail, thumbnailHeader, err := r.FormFile("thumbnail")
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "File cant be retreived", err)
+		return
+	}
+	defer uploadedThumbnail.Close()
+
+	mediatype, err := getMediaType(thumbnailHeader.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid media type", err)
+		return
+	}
+	if mediatype != "image/png" && mediatype != "image/jpeg" {
+		respondWithError(w, http.StatusBadRequest, "invalid Only jpeg or png supported", nil)
+		return
+	}
+
+	assetPath, err := getAssetPath(videoID, mediatype)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "invalid media type", err)
+		return
+	}
+
+	filepath := cfg.getAssetDiskPath(assetPath)
+
+	fmt.Println(filepath)
+
+	file, err := os.Create(filepath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "File can't be created", err)
+		return
+	}
+
+	_, err = io.Copy(file, uploadedThumbnail)
+	if err != nil {
+		os.Remove(filepath)
+		respondWithError(w, http.StatusInternalServerError, "File can't be saved", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Can't fetch video data", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Not authorized to update this video", nil)
+		return
+	}
+
+	assetURL := cfg.getAssetURL(assetPath)
+
+	video.ThumbnailURL = &assetURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Can't save thumbnail", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
